cc48eac3d9f7c2f37801f79bd3afa249cb41e274
Shubham Salunkhe (Shubham) <shubham.salunkhe@redwood.com>
2024-03-28 19:18:58 +0530

    Creating load-balancer with autoscaling

---
+++ launch_configuration.tf
@@ -0,0 +1,12 @@
+resource "aws_launch_configuration" "lc" {
+  name_prefix   = "tcw_lc"
+  image_id      = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+  user_data = data.template_file.user_data.rendered
+  associate_public_ip_address = true
+  security_groups    = [data.aws_security_group.tcw_sg.id]
+  
+  #lifecycle {
+  #  create_before_destroy = true
+  #}
+}
